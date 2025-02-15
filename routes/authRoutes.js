3b32689531b255babe41629f0eadc60d4631c840
anjum44 <anjumafrin303@gmail.com>
2025-02-15 23:03:37 +0600

    middlerware

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,17 @@
 const express = require("express");
-const { registerController, loginController } = require("../controllers/authController");
+const { registerController,loginController, currentUserController, } = require("../controllers/authController");
+const authMiddelware = require("../middlewares/authMiddelware");
+
 
 const router = express.Router()
 
-//
+//routes
 router.post('/register',registerController)
 
 // login route
 router.post("/login", loginController);
+
+//get current user
+router.get('/current-user',authMiddelware,currentUserController)
 
 module.exports = router;
