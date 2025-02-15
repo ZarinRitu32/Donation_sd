c489544692b512505727d16a98fb3c84aae3d26e
shamima50 <riashamima16@gmail.com>
2025-02-15 22:02:26 +0600

    hhhiii2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { registerController } = require("../controllers/authController");
+const { registerController, loginController } = require("../controllers/authController");
 
 const router = express.Router()
 
