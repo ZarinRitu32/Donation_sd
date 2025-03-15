5f9bbf0fe4f2e868ade5d8d09f5c3d6c18ce98b7
shamima50 <riashamima16@gmail.com>
2025-03-15 11:07:57 +0600

    aaapp

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -49,7 +49,7 @@ function App() {
           }
         />
         <Route
-          path="/orgnaisation"
+          path="/organisation"
           element={<ProtectedRoute>{<Organisation />}</ProtectedRoute>}
         />
         <Route
